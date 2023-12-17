bf46b75876ced524d4888e50e552ab2b5b63cae3
lucymao-12 <lucymao12@gmail.com>
2023-12-17 17:26:29 -0500

    added logout button and redirect back to login screen if user not logged in, need content and added security!

--- WebProject/client/src/components/Navbar.jsx
+++ WebProject/client/src/components/Navbar.jsx
@@ -3,16 +3,23 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useNavigate } from "react-router-dom";
 
 function Navbar(props) {
-    const nav = useNavigate()
-    function handleClick(event){
-        const classNames = event.target.className.split(' ');
-        const classNameHome = classNames.find((name) => name === "Home")
-        if(classNameHome){
-            nav("/home");
-        }else if(classNames.find((name) => name === "Start")){
-          nav("/chapters/0")
-        }
+  const nav = useNavigate();
+  function handleClick(event) {
+    const classNames = event.target.className.split(" ");
+    const classNameHome = classNames.find((name) => name === "Home");
+    if (classNameHome) {
+      if(localStorage.getItem("token") === null){
+        nav("/");
+      }else{
+        nav("/home");
+      }
+    } else if (classNames.find((name) => name === "Start")) {
+      nav("/chapters/0");
+    } else if (classNames.find((name) => name === "logout")) {
+      localStorage.removeItem("token");
+      nav("/");
     }
+  }
   return (
     <nav className="navbar navbar-expand navbar-light bg-light">
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -28,8 +35,11 @@ function Navbar(props) {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link">
-              About
+            <a className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link logout" onClick={handleClick}>
+              Logout
             </a>
           </li>
         </ul>
